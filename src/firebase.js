4732f50ba482dc0790081f00e3ff33dc95063ba7
Hervé <contact@jsapp.me>
2017-01-28 15:21:26 -0600

    remove default values

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJfI1NNWzsAotQ8rRUp_PLa9T7k0ZSplY",
-  authDomain: "myapp-1dbb8.firebaseapp.com",
-  databaseURL: "https://myapp-1dbb8.firebaseio.com",
-  storageBucket: "myapp-1dbb8.appspot.com",
+  apiKey: "xxxxx",
+  authDomain: "xxxx.firebaseapp.com",
+  databaseURL: "https://xxxx.firebaseio.com",
+  storageBucket: "xxxx.appspot.com",
 }
 export const firebaseApp = firebase.initializeApp(firebaseConfig)

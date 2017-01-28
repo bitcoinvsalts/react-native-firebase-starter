7ca80287c6c19435c249008665ae9127ea3b1c74
Hervé <contact@jsapp.me>
2017-01-28 15:54:09 -0600

    test commit

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import appStore from './store/AppStore'
 
 class App extends Component {
   componentDidMount() {
-    console.log("-------x------ App - componentDidMount ------x-------")
+    console.log("-------xx------ App - componentDidMount ------xx-------")
     OneSignal.inFocusDisplaying(0)
     OneSignal.configure({
       onNotificationReceived: function(notification) {
